2813e248164a7f831ada276e14d987a3699238fc
Mr <lsjsss@live.cn>
2023-02-10 14:03:10 +0800

    Update test

--- resume/js/script.js
+++ resume/js/script.js
@@ -85,8 +85,29 @@
 
 function printdata() {
   window.document.body.innerHTML=window.document.getElementById("print").innerHTML;
+  if (!!window.ActiveXObject || "ActiveXObject" in window) {
+    pagesetup_null();
+  }
   window.print();
-  location.reload()
+  location.reload();
+}
+ function pri() {
+    if (!!window.ActiveXObject || "ActiveXObject" in window) {
+        pagesetup_null();
+    }
+    window.print();
+}
+function pagesetup_null() {
+    var hkey_root, hkey_path, hkey_key;
+    hkey_root = "HKEY_CURRENT_USER";
+    hkey_path = "\\Software\\Microsoft\\Internet Explorer\\PageSetup\\";
+    try {
+        var RegWsh = new ActiveXObject("WScript.Shell");
+        hkey_key = "header";
+        RegWsh.RegWrite(hkey_root + hkey_path + hkey_key, "");
+        hkey_key = "footer";
+        RegWsh.RegWrite(hkey_root + hkey_path + hkey_key, "");
+    } catch (e) { }
 }
 
 
